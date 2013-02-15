c62738d9eb6d24a064ee901cfc3caef355fa0f1a
Gilles Louppe <g.louppe@gmail.com>
2013-02-15 13:25:17 +0100

    Use matplotlib.mlab.specgram

--- features.py
+++ features.py
@@ -5,13 +5,15 @@ import numpy as np
 
 from sklearn.cross_validation import cross_val_score
 from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.linear_model import SGDClassifier
 
 data = np.load("data/train-subsample.npz")
 X = data["X_train"]
 y = data["y_train"]
 
-clf = ExtraTreesClassifier(n_estimators=1, max_features=None, n_jobs=1)
-cv = 2
+clf = ExtraTreesClassifier(n_estimators=100, max_features=None, n_jobs=1)
+# clf = SGDClassifier()
+cv = 5
 
 
 # Test the goodness of features
@@ -53,11 +55,11 @@ scores = cross_val_score(clf, _X, _y, scoring="roc_auc", cv=cv)
 print "fft(raw).real + fft(raw).imag =", scores.mean()
 
 # Spectrogram
-import pylab as pl
+from matplotlib.mlab import specgram
 spectrogram = []
 
 for X_i in X:
-    s = pl.specgram(X_i)
+    s = specgram(X_i)
     spectrogram.append(s[0].flatten())
 
 _X = np.array(spectrogram)
