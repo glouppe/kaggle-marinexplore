808ce0c0d7305fa958289b6f1d5f01afb4983891
Gilles Louppe <g.louppe@gmail.com>
2013-02-16 10:32:23 +0100

    Restructure features.py into functions

--- features.py
+++ features.py
@@ -3,109 +3,128 @@
 
 import numpy as np
 
-from sklearn.cross_validation import cross_val_score
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.linear_model import SGDClassifier
+from matplotlib.mlab import specgram
 
-data = np.load("data/train-subsample.npz")
-X = data["X_train"]
-y = data["y_train"]
+from sklearn.preprocessing import normalize as normalize_
+from sklearn.random_projection import GaussianRandomProjection
+from sklearn.random_projection import SparseRandomProjection
 
-clf = ExtraTreesClassifier(n_estimators=1, max_features=None, n_jobs=1)
-# clf = SGDClassifier()
-cv = 2
-
-
-# Test the goodness of features
 
 # TODO:
 # - MFCC features
 # - DBNs features
 
+
 # Raw features
-_X = X
-_y = y
-scores = cross_val_score(clf, _X, _y, scoring="roc_auc", cv=cv)
-print "raw =", scores.mean()
+def raw(X):
+    return X
 
 # Normalized raw features
-from sklearn.preprocessing import normalize
-_X = normalize(X)
-_y = y
-scores = cross_val_score(clf, _X, _y, scoring="roc_auc", cv=cv)
-print "normalize(raw) =", scores.mean()
+def normalize(X):
+    return normalize_(X)
 
 # FFT
-_X = abs(np.fft.fft(X))
-_y = y
-scores = cross_val_score(clf, _X, _y, scoring="roc_auc", cv=cv)
-print "fft(raw) =", scores.mean()
+def fft(X):
+    return abs(np.fft.fft(X))
 
-# FFT of normalized
-_X = abs(np.fft.fft(normalize(X)))
-_y = y
-scores = cross_val_score(clf, _X, _y, scoring="roc_auc", cv=cv)
-print "fft(normalize(raw)) =", scores.mean()
-
-# FFT.real + FFT.imag
-fft = np.fft.fft(X)
-_X = np.hstack((fft.real, fft.imag))
-_y = y
-scores = cross_val_score(clf, _X, _y, scoring="roc_auc", cv=cv)
-print "fft(raw).real + fft(raw).imag =", scores.mean()
+# FFT real+imag
+def fft_realimag(X):
+    fft = np.fft.fft(X)
+    return np.hstack((fft.real, fft.imag))
 
 # Spectrogram
-from matplotlib.mlab import specgram
-spectrogram = []
-
-for X_i in X:
-    s = specgram(X_i, Fs=2000)
-    spectrogram.append(s[0].flatten())
-
-_X = np.array(spectrogram)
-_y = y
-
-scores = cross_val_score(clf, _X, _y, scoring="roc_auc", cv=cv)
-print "spectrogram =", scores.mean()
-
-# Spectrogram, lower than N hz
-from matplotlib.mlab import specgram
-
-for lower_bound in [50, 100, 200, 300, 400, 500, 1000]:
-    spectrogram = []
+def spectrogram(X, upper=None):
+    _X = []
 
     for X_i in X:
         s = specgram(X_i, Fs=2000)
         content = s[0]
         freqs = s[1]
-        spectrogram.append(content[freqs <= lower_bound].flatten())
 
-    _X = np.array(spectrogram)
-    _y = y
+        if upper is not None:
+            _X.append(content[freqs < upper].flatten())
+        else:
+            _X.append(content.flatten())
 
-    scores = cross_val_score(clf, _X, _y, scoring="roc_auc", cv=cv)
-    print "spectrogram[< %d Hz] =" % lower_bound, scores.mean()
+    return np.array(_X)
 
-# Random projection
-from sklearn.random_projection import GaussianRandomProjection
-from sklearn.random_projection import SparseRandomProjection
+# Summary statistics of the spectrogram
+def spectrogram_stats(X, upper=None):
+    _X = []
 
-_X = GaussianRandomProjection(eps=0.25).fit_transform(X)
-_y = y
-scores = cross_val_score(clf, _X, _y, scoring="roc_auc", cv=cv)
-print "GaussianRandomProjection(raw) =", scores.mean()
+    for X_i in X:
+        s = specgram(X_i, Fs=2000)
+        content = s[0]
+        freqs = s[1]
 
-_X = SparseRandomProjection(eps=0.25).fit_transform(X)
-_y = y
-scores = cross_val_score(clf, _X, _y, scoring="roc_auc", cv=cv)
-print "SparseRandomProjection(raw) =", scores.mean()
+        if upper is not None:
+            content = content[freqs < upper]
 
-_X = GaussianRandomProjection(eps=0.25).fit_transform(abs(np.fft.fft(X)))
-_y = y
-scores = cross_val_score(clf, _X, _y, scoring="roc_auc", cv=cv)
-print "GaussianRandomProjection(fft(raw)) =", scores.mean()
+        _X.append(np.hstack((content.min(axis=1), content.max(axis=1), content.mean(axis=1), np.median(content, axis=1))))
 
-_X = SparseRandomProjection(eps=0.25).fit_transform(abs(np.fft.fft(X)))
-_y = y
-scores = cross_val_score(clf, _X, _y, scoring="roc_auc", cv=cv)
-print "SparseRandomProjection(fft(raw)) =", scores.mean()
+    return np.array(_X)
+
+# Gaussian random projection
+def gaussian_random_projection(X, eps=0.25):
+    return GaussianRandomProjection(eps=eps).fit_transform(X)
+
+# Sparse random projection
+def sparse_random_projection(X, eps=0.25):
+    return SparseRandomProjection(eps=eps).fit_transform(X)
+
+
+if __name__ == "__main__":
+    from sklearn.cross_validation import cross_val_score
+    from sklearn.ensemble import ExtraTreesClassifier
+    from sklearn.linear_model import SGDClassifier
+
+    # Setup
+    data = np.load("data/train-subsample.npz")
+    X = data["X_train"]
+    y = data["y_train"]
+
+    clf = ExtraTreesClassifier(n_estimators=100, max_features=None, n_jobs=1)
+    # clf = SGDClassifier()
+    cv = 5
+
+    # Evaluate the goodness of all features
+    scores = cross_val_score(clf, raw(X), y, scoring="roc_auc", cv=cv)
+    print "X =", scores.mean()
+
+    scores = cross_val_score(clf, normalize(X), y, scoring="roc_auc", cv=cv)
+    print "normalize(X) =", scores.mean()
+
+    scores = cross_val_score(clf, fft(X), y, scoring="roc_auc", cv=cv)
+    print "fft(X) =", scores.mean()
+
+    scores = cross_val_score(clf, fft(normalize(X)), y, scoring="roc_auc", cv=cv)
+    print "fft(normalize(X)) =", scores.mean()
+
+    scores = cross_val_score(clf, fft_realimag(X), y, scoring="roc_auc", cv=cv)
+    print "fft_realimag(X) =", scores.mean()
+
+    scores = cross_val_score(clf, spectrogram(X), y, scoring="roc_auc", cv=cv)
+    print "spectrogram(X) =", scores.mean()
+
+    for upper in [50, 100, 200, 300, 400, 500, 1000]:
+        scores = cross_val_score(clf, spectrogram(X, upper=upper), y, scoring="roc_auc", cv=cv)
+        print "spectrogram(X, upper=%d) =" % upper, scores.mean()
+
+    scores = cross_val_score(clf, spectrogram_stats(X), y, scoring="roc_auc", cv=cv)
+    print "spectrogram_stats(X) =", scores.mean()
+
+    for upper in [50, 100, 200, 300, 400, 500, 1000]:
+        scores = cross_val_score(clf, spectrogram_stats(X, upper=upper), y, scoring="roc_auc", cv=cv)
+        print "spectrogram_stats(X, upper=%d) =" % upper, scores.mean()
+
+    scores = cross_val_score(clf, gaussian_random_projection(X), y, scoring="roc_auc", cv=cv)
+    print "gaussian_random_projection(X) =", scores.mean()
+
+    scores = cross_val_score(clf, gaussian_random_projection(fft(X)), y, scoring="roc_auc", cv=cv)
+    print "gaussian_random_projection(fft(X)) =", scores.mean()
+
+    scores = cross_val_score(clf, sparse_random_projection(X), y, scoring="roc_auc", cv=cv)
+    print "sparse_random_projection(X) =", scores.mean()
+
+    scores = cross_val_score(clf, sparse_random_projection(fft(X)), y, scoring="roc_auc", cv=cv)
+    print "sparse_random_projection(fft(X)) =", scores.mean()
