a308f2c1d351fb9f8d378d0949400f0f68c04ee9
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2013-02-24 00:04:15 +0100

    add build_gbrt

--- grid.py
+++ grid.py
@@ -42,7 +42,8 @@ def load_data():
     X = np.hstack((X_specs, X_ceps, X_mfcc))
 
     # Split into train/test
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5,
+                                                        random_state=42)
 
     return X_train, X_test, y_train, y_test
 
@@ -57,6 +58,22 @@ def build_extratrees(argv):
     }
 
     clf = ExtraTreesClassifier(**parameters)
+
+    return clf
+
+
+def build_gbrt(argv):
+    from sklearn.ensemble import GradientBoostingClassifier
+
+    parameters = {
+        "n_estimators": int(argv[0]),
+        "max_depth": int(argv[1]),
+        "learning_rate": float(argv[2]),
+        "max_features": int(argv[3]),
+        "min_samples_split": int(argv[4]),
+    }
+
+    clf = GradientBoostingClassifier(**parameters)
 
     return clf
 
