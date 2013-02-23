ac7fa6cf3572358879a0050bb492be4d0e47552c
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2013-02-23 19:40:44 +0100

    hacking

--- evaluate.py
+++ evaluate.py
@@ -4,6 +4,7 @@ from sklearn.cross_validation import cross_val_score
 from sklearn.svm import LinearSVC, SVC
 from sklearn.linear_model import SGDClassifier
 from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.decomposition import PCA
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.preprocessing import StandardScaler
@@ -12,25 +13,38 @@ from transform import FlattenTransformer
 from transform import SpectrogramTransformer
 from transform import SpectrogramStatsTransformer
 from ranking import RankSVM
+from ranking import SVMPerf
+from ranking import RGradientBoostingClassifier
 
 from sklearn.grid_search import GridSearchCV
 
 import IPython
 
-data = np.load("data/train_small.npz")
+data = np.load("data/train.npz")
 
 X = data["X_train"]
 y = data["y_train"]
 
-## clf = GradientBoostingClassifier(n_estimators=500, max_depth=4,
-##                                  min_samples_leaf=7, learning_rate=0.2)
-
+clf = GradientBoostingClassifier(n_estimators=500, max_depth=3,
+                                 min_samples_leaf=3, learning_rate=0.1)
+## clf = ExtraTreesClassifier(n_estimators=100, min_samples_split=1, max_features=None)
 # optimal C for raw w/o scaling is 0.00001
 # optimal C for stats w/o scaling is 0.00001
-clf = LinearSVC(C=0.00001, loss='l1', dual=True)
+clf = LinearSVC(C=0.00001, loss='l1', dual=True, class_weight='auto')
+## clf = RGradientBoostingClassifier({"distribution": "auc",
+##                                    "shrinkage": 0.1,
+##                                    "n.tree": 200,
+##                                    "bag.fraction": 1.0,
+##                                    "verbose": True,
+##                                    "n.minobsinnode": 1,
+##                                    "interaction.depth": 2,
+##                                    "offset": False})
+
+## clf = SVMPerf(C=0.00001, verbose=0, tol=0.001)
 ## clf = Pipeline(steps=[('scale', StandardScaler()),
 ##                       ('svm', clf)])
-st = SpectrogramTransformer(NFFT=256, clip=500, noverlap=0.6, dtype=np.float64,
+st = SpectrogramTransformer(NFFT=256, clip_upper=500, clip_lower=0,
+                            noverlap=0.6, dtype=np.float64,
                             whiten=None, log=True, flatten=False)
 X = st.fit_transform(X)
 
@@ -42,10 +56,42 @@ tf = FeatureUnion([
 
 X = tf.fit_transform(X)
 
+clf.fit(X, y)
+
+## data = np.load("data/test.npz")
+## X_test = data["X_test"]
+
+## X_test = st.fit_transform(X_test)
+## X_test = tf.fit_transform(X_test)
+
+## y_scores = clf.decision_function(X_test)
+## np.savetxt("linsvm1.txt", y_scores)
+
 print X.shape
+print st
 print clf
-scores = cross_val_score(clf, X, y, scoring="roc_auc", cv=5, n_jobs=3)
+scores = cross_val_score(clf, X, y, scoring="roc_auc", cv=5, n_jobs=2)
 print("spectrogram: %.5f (%.5f)" % (scores.mean(), scores.std()))
+
+## param_grid = {'C': [0.0001, 0.00001, 0.000001, 0.0000001],
+##               }
+
+## grid_search = GridSearchCV(clf, param_grid, scoring='roc_auc', n_jobs=2, iid=True,
+##                            refit=True, cv=5, verbose=0, pre_dispatch='2*n_jobs')
+
+
+## print('_' * 80)
+## t0 = time()
+## grid_search.fit(X, y)
+## print("done in %0.3fs" % (time() - t0))
+## print
+
+## print("Best score: %0.3f" % grid_search.best_score_)
+## print("Best parameters set:")
+## best_parameters = grid_search.best_estimator_.get_params()
+## for param_name in sorted(param_grid.keys()):
+##     print("\t%s: %r" % (param_name, best_parameters[param_name]))
+
 
 ## print "done"
 ## clf = SGDClassifier(n_iter=10, alpha=0.001, loss='hinge',
